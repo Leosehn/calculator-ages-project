8910063d86dd54328e6cbfac4413118211deac63
leosehn <leonardo.santos007@edu.pucrs.br>
2025-03-13 15:23:17 -0300

    parte do back feito algumas coisas para alterar tema feito

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,3 +1,72 @@
 function execButton(objValue) {
+    if(objValue == "clear") {
+        document.getElementById("text_0").value = "";
+        document.getElementById("text_store").value = ""
+        return;
+    }
+    else if(objValue == "percent"){
+        document.getElementById("text_0").value += "%"
+        document.getElementById("text_store").value += "/100"
+        return
+    }
+    else if(objValue == "*") {
+        document.getElementById("text_0").value += "x";
+        document.getElementById("text_store").value += "*"
+        return
+    }
+    else if(objValue == "/") {
+        document.getElementById("text_0").value += "÷"
+        document.getElementById("text_store").value += "/"
+        return
+    }
+    else if(objValue == "invert") {
+        document.getElementById("text_0") += "-"
+        document.getElementById("text_store") += "-"
+        return
+    }
+    else if(objValue == "equals") {
+        result = operate(document.getElementById("text_0").value)
+        document.getElementById("text_1").value = document.getElementById("text_0").value
+        document.getElementById("text_0").value = result
+        document.getElementById("text_store").value = result
+    }
+    else if (objValue == "remove") {
+        document.getElementById("text_0").value = removeLastCharacter(document.getElementById("text_0").value)
+        document.getElementById("text_store").value = removeLastCharacter(document.getElementById("text_store").value)
+        return
+    }
+    else if (objValue == ".") {
+        //if ()
+    }
+    document.getElementById("text_0").value += objValue;
+    document.getElementById("text_store").value += objValue;
+}
+
+function removeLastCharacter(value) {
+    newString = ""
+    for (let i = 0; i < length(value); i++) {
+        if (i != length(value) - 2) {
+            newString += value.charAt(i);
+        }
+        else {
+            return newString;
+        }
+    }
+}
+
+function operate(elementValue) {
+    
+    for (let i = 0; i < length(elementValue); i++) {
+        let character = elementValue.charAt(i);
+        if (character == "x") {
+            
+        }
+        if (i == length(elementValue) - 1) {
+
+        }
+    }
+}
+
+function mult() {
 
 }
